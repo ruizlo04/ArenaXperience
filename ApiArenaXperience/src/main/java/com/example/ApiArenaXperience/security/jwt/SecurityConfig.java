3ec66da06d23b00a3a5c7815412e37ed4a6cecfe
ruizlo04 <carlo@CalosRuizLopez>
2025-02-20 19:18:06 +0100

    login with the register user

--- ApiArenaXperience/src/main/java/com/example/ApiArenaXperience/security/jwt/SecurityConfig.java
+++ ApiArenaXperience/src/main/java/com/example/ApiArenaXperience/security/jwt/SecurityConfig.java
@@ -66,7 +66,7 @@ public class SecurityConfig {
                 .accessDeniedHandler(accessDeniedHandler)
         );
         http.authorizeHttpRequests(authz -> authz
-                .requestMatchers(HttpMethod.POST, "/auth/register", "/auth/login", "/auth/refresh/token", "/error").permitAll()
+                .requestMatchers(HttpMethod.POST, "/auth/register", "/auth/login", "/auth/refresh/token", "/error", "/activate/account/").permitAll()
                 .requestMatchers("/me/admin").hasRole("ADMIN")
                 .requestMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated());
