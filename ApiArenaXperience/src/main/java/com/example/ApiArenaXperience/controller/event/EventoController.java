64c02d09f113cad2285d5ce42f1540c1010c3471
ruizlo04 <carlo@CalosRuizLopez>
2025-02-23 12:16:19 +0100

    list events done and added some examples.

--- ApiArenaXperience/src/main/java/com/example/ApiArenaXperience/controller/event/EventoController.java
+++ ApiArenaXperience/src/main/java/com/example/ApiArenaXperience/controller/event/EventoController.java
@@ -5,6 +5,7 @@ import com.example.ApiArenaXperience.service.event.EventoService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,6 +18,7 @@ public class EventoController {
 
     private final EventoService eventoService;
 
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/events")
     public ResponseEntity<List<EventoResponse>> getAllEvents() {
         return ResponseEntity.ok(eventoService.getAllEvents());
