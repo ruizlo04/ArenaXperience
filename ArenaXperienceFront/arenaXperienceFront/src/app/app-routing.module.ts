00e15695de217581f985ac13e76457faf42a3abb
ruizlo04 <carlo@CalosRuizLopez>
2025-06-14 12:42:37 +0200

    registro completado con validaciones

--- ArenaXperienceFront/arenaXperienceFront/src/app/app-routing.module.ts
+++ ArenaXperienceFront/arenaXperienceFront/src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/auth/login/login.component';
 import { NavInicioSesionComponent } from './components/navbars/nav-inicio-sesion/nav-inicio-sesion.component';
 import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { ActivateAccountComponent } from './components/auth/activate-account/activate-account.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'nav-incio-sesion', component: NavInicioSesionComponent },
+  { path: 'activate-account', component: ActivateAccountComponent },
   { path: 'home', component: HomeComponent }
 
 ];
