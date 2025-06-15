b5cf9b6ec89a3928ff5d6d9fc3db8a1f1de1e470
ruizlo04 <carlo@CalosRuizLopez>
2025-06-15 17:43:20 +0200

    conexion done

--- ApiArenaXperience/src/main/java/com/example/ApiArenaXperience/dto/user/UserResponse.java
+++ ApiArenaXperience/src/main/java/com/example/ApiArenaXperience/dto/user/UserResponse.java
@@ -1,26 +1,49 @@
 package com.example.ApiArenaXperience.dto.user;
 
+import com.example.ApiArenaXperience.model.user.UserRole;
 import com.example.ApiArenaXperience.model.user.Usuario;
-import com.example.ApiArenaXperience.validation.UniqueUsername;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
+import java.time.Instant;
+import java.util.Set;
 import java.util.UUID;
 
 public record UserResponse(
         UUID id,
-        @UniqueUsername
         String username,
+        String email,
+        String phoneNumber,
+        Set<UserRole> roles,
+        Instant createdAt,
         @JsonInclude(JsonInclude.Include.NON_NULL)
         String token,
         @JsonInclude(JsonInclude.Include.NON_NULL)
         String refreshToken
 ) {
 
-    public static UserResponse of (Usuario user) {
-        return new UserResponse(user.getId(), user.getUsername(), null, null);
+    public static UserResponse of(Usuario user) {
+        return new UserResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPhoneNumber(),
+                user.getRoles(),
+                user.getCreatedAt(),
+                null,
+                null
+        );
     }
 
-    public static UserResponse of (Usuario user, String token, String refreshToken) {
-        return new UserResponse(user.getId(), user.getUsername(), token, refreshToken);
+    public static UserResponse of(Usuario user, String token, String refreshToken) {
+        return new UserResponse(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPhoneNumber(),
+                user.getRoles(),
+                user.getCreatedAt(),
+                token,
+                refreshToken
+        );
     }
 }
