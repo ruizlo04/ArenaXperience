cb4a5cd44bff71985474bd4da24ba0aea4fd4782
ruizlo04 <carlo@CalosRuizLopez>
2025-06-16 21:20:54 +0200

    avances para editar el perfil

--- ArenaXperienceFront/arenaXperienceFront/src/app/components/auth/profile/profile.component.ts
+++ ArenaXperienceFront/arenaXperienceFront/src/app/components/auth/profile/profile.component.ts
@@ -8,12 +8,18 @@ import { AuthService } from '../../../services/auth.service';
 })
 export class ProfileComponent implements OnInit {
   user: any = null;
+  editedUser: any = {};
   loading = true;
   error = '';
+  editing = false;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.loadProfile();
+  }
+
+  loadProfile(): void {
     this.authService.getProfile().subscribe({
       next: (data) => {
         this.user = data;
@@ -22,6 +28,45 @@ export class ProfileComponent implements OnInit {
       error: () => {
         this.error = 'No se pudo cargar el perfil.';
         this.loading = false;
+      }
+    });
+  }
+
+  toggleEdit(): void {
+    this.editing = !this.editing;
+    if (this.editing) {
+      this.editedUser = {
+        email: this.user.email,
+        verifyEmail: this.user.email,
+        password: '',
+        verifyPassword: '',
+        phoneNumber: this.user.phoneNumber
+      };
+    }
+  }
+
+  saveChanges(): void {
+    if (this.editedUser.email !== this.editedUser.verifyEmail) {
+      alert('Los emails no coinciden.');
+      return;
+    }
+
+    if (this.editedUser.password !== this.editedUser.verifyPassword) {
+      alert('Las contraseñas no coinciden.');
+      return;
+    }
+
+    const username = this.user.username;
+
+    this.authService.editUser(username, this.editedUser).subscribe({
+      next: (updatedUser) => {
+        this.user = updatedUser;
+        this.editing = false;
+        alert('Perfil actualizado correctamente');
+      },
+      error: (err) => {
+        console.error(err);
+        alert('Hubo un error al actualizar el perfil.');
       }
     });
   }
